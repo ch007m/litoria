0ca9e5f21463cdee36b044f44e0a3b8c6c3f2fc9
Charles Moulliard <cmoulliard@apache.org>
2016-08-18 18:30:59 +0200

    Add test cases

---
+++ test/render.js
@@ -0,0 +1,39 @@
+var test = require('tape'),
+    asciidoctor = require('asciidoctor.js')(),
+    fs = require('fs'),
+    path = require('path'),
+    gutil = require('gulp-util');
+
+var opal = asciidoctor.Opal;
+processor = asciidoctor.Asciidoctor();
+
+function getFile(filePath) {
+    return new gutil.File({
+        path: path.resolve(filePath),
+        cwd: './test/',
+        base: path.dirname(filePath),
+        contents: new Buffer(String(fs.readFileSync(filePath)))
+    });
+}
+
+test('Convert adoc string to HTML using doctype : inline', function(assert) {
+    var content = "http://asciidoctor.org[*Asciidoctor*] " +
+        "running on http://opalrb.org[_Opal_] " +
+        "brings AsciiDoc to the browser!";
+    const expected = '<a href="http://asciidoctor.org"><strong>Asciidoctor</strong></a> running on <a href="http://opalrb.org"><em>Opal</em></a> brings AsciiDoc to the browser!'
+    var options = opal.hash({doctype: 'inline', attributes: ['showtitle']});
+    var result = opal.Asciidoctor.$convert(content, options);
+
+    assert.equal(result, expected,"Render to HTML");
+    assert.end();
+});
+
+test('Convert adoc string to HTML using doctype : article, header_footer : true', function(assert) {
+    var content = getFile(path.join('test', 'fixtures', 'simple.adoc'));
+    const expected = getFile(path.join('test', 'fixtures', 'simple.html'));
+    var options = opal.hash({doctype: 'article', header_footer: 'true', attributes: ['nofooter']});
+    var result = opal.Asciidoctor.$convert(content, options);
+
+    assert.equal(result, expected,"Render to HTML");
+    assert.end();
+});
