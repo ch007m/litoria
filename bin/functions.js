fca80d74b0d71e8d49adad8c128bf4de01649b75
Charles Moulliard <cmoulliard@apache.org>
2016-08-22 18:05:27 +0200

    Refactor global functions

--- bin/functions.js
+++ bin/functions.js
@@ -1,27 +1,32 @@
+'use strict';
+
 module.exports = {
-    listPackages: function(pkgs,program) {
-        if (!pkgs.length) {
-            console.error('packages required');
-            process.exit(1);
-        }
-
-        if (program.force)
-            console.log('  force: install');
-        pkgs.forEach(function(pkg){
-            console.log('  install : %s', pkg);
-        });
-    },
-
-    listDirectories: function(pkgs,program) {
-        if (!pkgs.length) {
-            console.error('packages required');
-            process.exit(1);
-        }
-
-        if (program.force)
-            console.log('  force: install');
-        pkgs.forEach(function(pkg){
-            console.log('  install : %s', pkg);
-        });
-    }
+    listPackages: listPackages,
+    listDirectories: listDirectories
 };
+
+function listPackages(pkgs,program) {
+    if (!pkgs.length) {
+        console.error('packages required');
+        process.exit(1);
+    }
+
+    if (program.force)
+        console.log('  force: install');
+    pkgs.forEach(function(pkg){
+        console.log('  install : %s', pkg);
+    });
+}
+
+function listDirectories(pkgs,program) {
+    if (!pkgs.length) {
+        console.error('packages required');
+        process.exit(1);
+    }
+
+    if (program.force)
+        console.log('  force: install');
+    pkgs.forEach(function(pkg){
+        console.log('  install : %s', pkg);
+    });
+}
