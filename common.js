9291fe871bba92ceefb0a6abae4a9ca7e39326e1
Charles Moulliard <cmoulliard@apache.org>
2016-08-23 14:54:15 +0200

    Add makeRed function

--- common.js
+++ common.js
@@ -1,4 +1,13 @@
 // Parse package.json file to read config
 var data = require('./package.json');
+var colors = require('colors');
 
-module.exports = data;
+module.exports = {
+    data: data,
+    makeRed: makeRed
+};
+
+
+function makeRed(txt) {
+    return colors.red(txt); //display the help text in red on the console
+}
