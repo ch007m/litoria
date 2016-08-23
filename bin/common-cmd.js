b4ce079ebf258d5c429e4f04659c58817477cc36
Charles Moulliard <cmoulliard@apache.org>
2016-08-23 13:03:48 +0200

    Check if a command is passed. If this is not the case, then exit

--- bin/common-cmd.js
+++ bin/common-cmd.js
@@ -1,5 +1,6 @@
-var project = require('../common.js'),
-    program = require('commander');
+var project = require('../common.js');
+var program = require('commander');
+var colors  = require('colors');
 
 module.exports = program;
 
@@ -8,5 +9,13 @@ module.exports = program;
  */
 program
     .version(project.version)
-    .description(project.description)
-    .option('-v, --verbose', 'be verbose');
+    .description(project.description);
+
+    if (!process.argv.slice(2).length) {
+        program.outputHelp(make_red);
+        process.exit(0);
+    }
+
+function make_red(txt) {
+    return colors.red(txt); //display the help text in red on the console
+}
