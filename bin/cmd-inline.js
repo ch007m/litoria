9aa581db29e8fc88729e858a44fe78ea9a3971f7
Charles Moulliard <cmoulliard@apache.org>
2016-08-23 13:15:35 +0200

    Add help description to the command & move code to be called

--- bin/cmd-inline.js
+++ bin/cmd-inline.js
@@ -6,15 +6,23 @@ var program = require('./common-cmd.js'),
  * Generate command
  */
 program
+    .description('Inline the css')
     .option('-f, --file', 'path of the html file to inline')
-    .parse(process.argv);
+    .action(function () {
+        /*
+         * Call function responsible to convert the Asciidoc file(s) to HTML
+         */
+        if ($.isEmpty(program.args)) {
+            console.log("No arguments have been passed to the command.");
+            process.exit(0);
+        } else {
+            $.inline(program.args);
+        }
+    }).on('--help', function () {
+       console.log('  Examples:');
+       console.log();
+       console.log('    $ cmd inline config.yaml');
+       console.log();
+    });
 
-/*
- * Call function responsible to convert the Asciidoc file(s) to HTML
- */
-if ($.isEmpty(program.args)) {
-    console.log("No arguments have been passed to the command.");
-    process.exit(0);
-} else {
-    $.inline(program.args);
-}
+program.parse(process.argv);
