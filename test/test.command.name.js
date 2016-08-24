af331cc7d1fe01e050e4630e18ab79ac5bcc118e
Charles Moulliard <cmoulliard@apache.org>
2016-08-24 14:44:13 +0200

    Comment code. The test is not working correctly

--- test/test.command.name.js
+++ test/test.command.name.js
@@ -1,48 +1,29 @@
 var test = require('tape');
 var program = require('commander');
-var sinon = require('sinon').sandbox.create();
+// var sinon = require('sinon').sandbox.create();
 
-test("Simple command test", function (t) {
-
-    sinon.stub(process, 'exit');
-    sinon.stub(process.stdout, 'write');
-
-    program
-        .command('mycommand [options]', 'this is my command');
-
-    program.parse(['node', 'test']);
-
-    t.ok(typeof program.name === 'function')
-    t.strictEquals(program.name(), 'test')
-    t.equals(program.commands[0].name(), 'mycommand');
-    t.equals(program.commands[1].name(), 'help');
+test("Commander", function(t) {
 
     /*
-
-     tape test/test.command.name.js
-     TAP version 13
-     # Simple command test
-
-     error: unknown option `--help'
-
-     not ok 5 test exited without ending
-     ---
-     operator: fail
-     ...
-
-     1..5
-     # tests 5
-     # pass  4
-     # fail  1
-
-     */
-
-    /*  TODO
-    var output = process.stdout.write.args[0];
-    t.equals(output[0],'    mycommand [options]  this is my command');
+    sinon.stub(process, 'exit');
+    sinon.stub(process.stdout, 'write');
     */
 
-    t.end;
+    // ,{noHelp: true}
 
-    sinon.restore();
+    program.command('mycommand [options]','this is my command')
+           .version('0.0.1');
+
+    program.parse(['test']);
+
+    t.true(typeof program.name === 'function');
+    t.equal(program.version(), '0.0.1');
+    t.equal(program.commands[0].name(), 'mycommand');
+    t.equal(program.commands[1].name(), 'help');
+
+    //var output = process.stdout.write.args[0];
+    // t.equal(output[0],'');
+
+    t.end();
+    // sinon.restore();
 });
