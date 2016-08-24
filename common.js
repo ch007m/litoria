23f037a05aa3ac19aee38af9a7faac8c75540660
Charles Moulliard <cmoulliard@apache.org>
2016-08-24 14:43:30 +0200

    Remove makeRed as we are using chalk to colorize the output console

--- common.js
+++ common.js
@@ -1,12 +1,6 @@
 // Parse package.json file to read config
 var data = require('./package.json');
-var colors = require('colors');
 
 module.exports = {
-    data: data,
-    makeRed: makeRed,
+    data: data
 };
-
-function makeRed(txt) {
-    return colors.red(txt); //display the help text in red on the console
-}
