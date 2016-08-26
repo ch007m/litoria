fc1666e3c5cbc1ff1065ee87cb51d5774f67eeef
Charles Moulliard <cmoulliard@apache.org>
2016-08-26 11:54:50 +0200

    Add css styles to the input folder. Refactor code to use convert_file.

--- bin/litoria-inline.js
+++ bin/litoria-inline.js
@@ -14,7 +14,7 @@ program
             console.log(chalk.red("No arguments have been passed to the command."));
             process.exit(0);
         } else {
-            console.trace("File will be inlined !");
+            console.log("File will be inlined !");
             $.inline(program.args);
         }
     }).on('--help', function () {
