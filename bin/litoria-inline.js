00f980133fb1739040cbb02beecc5d6e8612c696
Charles Moulliard <cmoulliard@apache.org>
2016-09-02 17:47:01 +0200

    Refactor inline command to remove the action from the command

--- bin/litoria-inline.js
+++ bin/litoria-inline.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var program = require('commander');
-var $       = require('../lib/litoria.js');
-var Log     = require("../lib/log");
+var $ = require('../lib/litoria.js');
+var Log = require("../lib/log");
 
 var log = new Log();
 
@@ -11,19 +11,18 @@ var log = new Log();
 program
     .description('Inline the css content of a file')
     .usage('litoria inline')
-    .action(function () {
-        if ($.isEmpty(program.args)) {
-            log.error("No arguments have been passed to the command.");
-            process.exit(0);
-        } else {
-            log.info("File will be inlined !");
-            $.inline(program.args);
-        }
-    }).on('--help', function () {
-       log.info('  Examples:');
-       log.info();
-       log.info('    $ litoria inline config.yaml');
-       log.info();
-    });
+    .on('--help', function () {
+        log.info('  Examples:');
+        log.info();
+        log.info('    $ litoria inline config.yaml');
+        log.info();
+    })
+    .parse(process.argv);
 
-program.parse(process.argv);
+if ($.isEmpty(program.args)) {
+    log.error("No arguments have been passed to the command.");
+    process.exit(0);
+} else {
+    log.info("File will be inlined !");
+    $.inline(program.args);
+}
