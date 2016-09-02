03e7fdbe398627febc080197492c532eeb3bd619
Charles Moulliard <cmoulliard@apache.org>
2016-09-02 17:27:15 +0200

    Refactor command

--- bin/litoria-send.js
+++ bin/litoria-send.js
@@ -11,18 +11,17 @@ var log = new Log();
 program
     .description('Send html content as body ohe mail to a sender')
     .usage('litoria send')
-    .action(function () {
-        if ($.isEmpty(program.args)) {
-            log.error("No arguments have been passed to the command.");
-            process.exit(0);
-        } else {
-            $.send(program.args);
-        }
-    }).on('--help', function () {
+    .on('--help', function () {
        log.info('  Examples:');
        log.info();
        log.info('    $ litoria send config.yaml');
        log.info();
-    });
+    })
+    .parse(process.argv);
 
-program.parse(process.argv);
+if ($.isEmpty(program.args)) {
+    log.error("No arguments have been passed to the command.");
+    process.exit(0);
+} else {
+    $.send(program.args);
+}
