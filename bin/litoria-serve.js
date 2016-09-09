cd70014d5cf5186a03afff1c1734f55a9b5583bb
Charles Moulliard <cmoulliard@apache.org>
2016-09-09 15:19:55 +0200

    Rename server command to serve

--- bin/litoria-serve.js
+++ bin/litoria-serve.js
@@ -10,12 +10,12 @@ var log = new Log();
  */
 program
     .description('Start a local HTTP Server hosting the content')
-    .usage('litoria server')
+    .usage('litoria serve')
     .option('-o, --open', 'open browser window after starting the server')
     .on('--help', function () {
       log.info('  Examples:');
       log.info();
-      log.info('    $ litoria server server-cfg.yaml');
+      log.info('    $ litoria serve server-cfg.yaml');
       log.info();
     })
     .parse(process.argv);
@@ -24,5 +24,5 @@ if ($.isEmpty(program.args)) {
   log.error('No arguments have been passed to the command.');
   process.exit(0);
 } else {
-  $.server(program.args, program.open !== undefined);
+  $.serve(program.args, program.open !== undefined);
 }
